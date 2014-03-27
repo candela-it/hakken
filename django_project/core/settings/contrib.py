1e8cde504ccfc5fad75c1800b4ffbd0f55e7f70e
Igor Crnković <icrni@candela-it.com>
2014-03-27 18:42:16 +0100

    added django social auth for osm user auth

--- django_project/core/settings/contrib.py
+++ django_project/core/settings/contrib.py
@@ -6,6 +6,7 @@ INSTALLED_APPS += (
     'south',
     'crispy_forms',
     'pipeline',
+    'social.apps.django_app.default',
 )
 
 # use underscore template function
@@ -13,3 +14,11 @@ PIPELINE_TEMPLATE_FUNC = '_.template'
 
 # enable cached storage - requires uglify.js (node.js)
 STATICFILES_STORAGE = 'pipeline.storage.PipelineCachedStorage'
+
+# python social auth required settings
+AUTHENTICATION_BACKENDS = (
+    'social.backends.openstreetmap.OpenStreetMapOAuth',
+)
+
+SOCIAL_AUTH_OPENSTREETMAP_KEY = 'xuq0sXds8n9Bi2E6wcs49Y9vLREYVixxQkjcU2TF'
+SOCIAL_AUTH_OPENSTREETMAP_SECRET = 'Hjpv6n9jm7ejSuB0N5TvPAznq5k69vAD9vzdyLzH'
