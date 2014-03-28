827b1d1b20dfea18e57b8367042be8cb0e21c039
Igor Crnković <icrni@candela-it.com>
2014-03-28 11:45:39 +0100

    added admin subpages

--- django_project/admin/urls.py
+++ django_project/admin/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
-from .views import HomeView
+from .views import HomeView, AddNew
 
 urlpatterns = patterns(
     '',
     # basic app views
     url(r'^admin$', HomeView.as_view(), name='admin_home'),
+    url(r'^admin/add$', AddNew.as_view(), name='admin_add'),
 )
--- django_project/admin/views.py
+++ django_project/admin/views.py
@@ -5,3 +5,7 @@ from django.views.generic.base import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'adminhome.html'
+
+
+class AddNew(TemplateView):
+    template_name = 'admin_add.html'
