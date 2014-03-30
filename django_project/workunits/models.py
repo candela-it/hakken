6fa33e693840f49e59d206d3a7ff44fdb0076d8c
Dražen Odobašić <dodobas@candela-it.com>
2014-03-30 20:40:38 +0200

    Add basic workunits models

--- django_project/workunits/models.py
+++ django_project/workunits/models.py
@@ -2,3 +2,27 @@ import logging
 logger = logging.getLogger(__name__)
 
 from django.db import models
+
+
+class WorkUnit(models.Model):
+    """
+    WorkUnit model references a Project and basic information about grid
+    """
+
+    x = models.IntegerField(help_text='WorkUnit X coordinate on the grid')
+    y = models.IntegerField(help_text='WorkUnit Y coordinate on the grid')
+    z = models.IntegerField(help_text='WorkUnit Zoom level on the grid')
+    project = models.ForeignKey('projects.Project')
+    locked = models.BooleanField(help_text='Locked tile cannot be picked')
+
+    def __unicode__(self):
+        return '{} ({}/{}/{})'.format(self.project, self.x, self.y, self.z)
+
+
+class Solution(models.Model):
+    """
+    Answered question for the WorkUnit
+    """
+    workunit = models.ForeignKey('workunits.WorkUnit')
+    answer = models.ForeignKey('projects.Answer')
+    user = models.ForeignKey('auth.User')
